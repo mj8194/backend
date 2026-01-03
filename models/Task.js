ef3cc9a213f388b847e19cd77d0f1999c96bdcc5
Subhiksha Jain <mj8194328@gmail.com>
2026-01-03 12:30:59 +0530

    Add backend source code

--- models/Task.js
+++ models/Task.js
@@ -1,29 +1,17 @@
 import mongoose from "mongoose";
 
+
 const taskSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, trim: true },
-    description: { type: String },
-
-    priority: {
-      type: String,
-      enum: ["Low", "Medium", "High"],
-      default: "Low",
-    },
-
-    dueDate: {
-      type: Date,
-      default: null,          // ✅ NOT required anymore
-    },
-
-    status: {
-      type: String,
-      enum: ["Pending", "Completed"],
-      default: "Pending",
-    },
+    title: { type: String, required: true, trim: true }, 
+    description: { type: String },                       
+    priority: { type: String, enum: ["Low", "Medium", "High"], default: "Low" },
+    dueDate: { type: Date, required: true },            
+    status: { type: String, enum: ["Pending", "Completed"], default: "Pending" } 
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
 const Task = mongoose.model("Task", taskSchema);
+
 export default Task;
